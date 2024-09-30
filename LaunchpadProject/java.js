99fa0f93654da40bb84018ba5e27ca6153df333e
Jenni4B <jennifer.ea.blocker@gmail.com>
2024-09-30 13:45:04 -0400

    added fish picture to the images in JS

--- LaunchpadProject/java.js
+++ LaunchpadProject/java.js
@@ -4,27 +4,21 @@ let images = [
         alt: "a pencil drawing of a girl with many braids"
     },
     {
-        src: "https://yt3.ggpht.com/yjX22O4IfHRn_q9qvAZbx1JhLoV07LCzWuwd48dc8l2HEmon2kWG38E96i6g_eU2NESBBpxucMNX=s288-c-fcrop64=1,0000063efffff9c1-rw-nd-v1", // Added missing comma
+        src: "https://yt3.ggpht.com/yjX22O4IfHRn_q9qvAZbx1JhLoV07LCzWuwd48dc8l2HEmon2kWG38E96i6g_eU2NESBBpxucMNX=s288-c-fcrop64=1,0000063efffff9c1-rw-nd-v1", 
         alt: "Colored pencil drawing of Usagi from Sailor Moon"
     },
     {
-        src: "https://yt3.ggpht.com/FNgjtFs5QkjvTCTvzgevDQUFVegYb5xk012P3IpV8_5UkXFpQ01-TgkyitHjXXQ2teUkfKv3Ec29LA=s288-c-fcrop64=1,09360000f6c9ffff-rw-nd-v1", // Added missing comma
+        src: "https://yt3.ggpht.com/FNgjtFs5QkjvTCTvzgevDQUFVegYb5xk012P3IpV8_5UkXFpQ01-TgkyitHjXXQ2teUkfKv3Ec29LA=s288-c-fcrop64=1,09360000f6c9ffff-rw-nd-v1", 
         alt: "pen drawing of an eye"
+    }
+    {
+        src: "https://64.media.tumblr.com/19ad4293e84c72f0cdc14016dc4777a6/f3ca7286bf353358-d4/s540x810/330af57d67178d1af7e7df98fc339ab279349c1a.pnj", 
+        alt: "a drawing of a fish"
     }
 ];
 
 
-// hiding image function 
-    // if hideButton is clicked, then image is removed from the seen until 
-    // the user undoes the decision
-
-// if user clicks the like button
-//      add to liked class
-
-// if user clicks the dislike,
-// add to dislike class
-
-// Instagram redirect
+// Instagram and github redirect
 let instagramLink = "https://www.instagram.com/jeanbeanbc/"
 let githubLink = "https://github.com/Jenni4B"
 
@@ -39,11 +33,14 @@ function github(){
 }
 
 function likedThePic(){
-    
+    // if user clicks the like button
+//      add to liked class
 }
 function dislikedThePic(){
-
+// if user clicks the dislike,
+// add to dislike class
 }
 function hideThePic(){
-
+// hiding image function 
+    // if hideButton is clicked, then image is removed from the seen
 }
